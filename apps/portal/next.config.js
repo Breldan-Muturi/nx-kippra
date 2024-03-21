633084154c3277c9c75ae6c33a591020ff095e82
Breldan-Muturi <breldan@sohnandsol.com>
2024-03-21 13:48:02 +0300

    Modified portal dockerfile

--- apps/portal/next.config.js
+++ apps/portal/next.config.js
@@ -15,23 +15,29 @@ const nextConfig = {
   images: {
     // domains: ['s3.amazonaws.com'],
     remotePatterns: [
+      // {
+      //   hostname: 'd1muf25xaso8hp.cloudfront.net',
+      // },
+      // {
+      //   hostname: 'kippra.or.ke',
+      // },
+      // {
+      //   hostname: 'utfs.io',
+      // },
+      // {
+      //   hostname: 's3.amazonaws.com',
+      //   port: '',
+      //   protocol: 'https',
+      //   pathname: '**',
+      // },
+      // {
+      //   hostname: 'lh3.googleusercontent.com',
+      // },
       {
-        hostname: 'd1muf25xaso8hp.cloudfront.net',
-      },
-      {
-        hostname: 'kippra.or.ke',
-      },
-      {
-        hostname: 'utfs.io',
-      },
-      {
-        hostname: 's3.amazonaws.com',
+        hostname: '**',
         port: '',
         protocol: 'https',
         pathname: '**',
-      },
-      {
-        hostname: 'lh3.googleusercontent.com',
       },
     ],
   },
